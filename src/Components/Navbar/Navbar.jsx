d5555f2de4e052eaed5ba791da02405b4791db94
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-05-27 18:17:14 +0530

    Add the navbar component

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import "./Navbar.css"
+import logo from "../../assets/logo.svg"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+    <img src={logo} alt="logo" />
+    <ul className="nav-menu">
+      <li>Home</li>
+      <li>About Me</li>
+      <li>Services</li>
+      <li>Portfolio</li>
+      <li>Contact</li>
+    </ul>
+
+    <div className="nav-connect">
+      Connect With Me
+    </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
