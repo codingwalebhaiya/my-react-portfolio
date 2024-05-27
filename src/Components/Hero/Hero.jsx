582dafdb9993ec17a8d59bab0097a64d4d587e27
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-05-28 00:22:15 +0530

    complete the Hero component

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,21 @@
 
 import "./Hero.css"
+import logo from "../../assets/logo.jpg"
 
 const Hero = () => {
   return (
     <div className="hero">
-      
+    <img src={logo} alt="" />
+    <h1> <span>I'm Satyam Pandey,</span> frontend developer based in INDIA.</h1>
+    <p>I'm a frontend developer from Noida , INDIA </p>
+     <div className="hero-action">
+      <div className="hero-connect">
+        Connect with me
+      </div>
+      <div className="hero-resume">
+        My resume
+      </div>
+     </div> 
     </div>
   )
 }
